c784edabccbdf1d4e8e2368f7d19aaa1d3b22438
Jonas <96282373+Volzor@users.noreply.github.com>
2022-12-18 01:59:01 -0800

    created the working game
    Created a loop to create 5 rounds of the game. Current user choice doesn't prompt anything yet.

--- index.js
+++ index.js
@@ -9,30 +9,38 @@ const getComputerChoice = () => {
 
 
 // function that takes player choice and compares it to the random computer choice using else if statement
-const playGame = (playerSelection, compSelection) => {
+const playRound = (playerSelection, compSelection) => {
     if (playerSelection == 'Rock' && compSelection == 'Scissors') {
-        return 'Player wins!'
+        return `Player wins! ${playerSelection} beats ${compSelection}`
     } else if (playerSelection == 'Scissors' && compSelection == 'Rock') {
-        return 'Computer wins!'
+        return `Computer wins! ${compSelection} beats ${playerSelection}`
     } else if (playerSelection == 'Paper' && compSelection == 'Rock') {
-        return 'Player wins!'
+        return `Player wins! ${playerSelection} beats ${compSelection}`
     } else if (playerSelection == 'Rock' && compSelection == 'Paper') {
-        return 'Computer wins!' 
+        return `Computer wins! ${compSelection} beats ${playerSelection}` 
     } else if (playerSelection == 'Scissors' && compSelection == 'Paper') {
-        return 'Player wins!' 
+        return `Player wins! ${playerSelection} beats ${compSelection}` 
     } else if (playerSelection == 'Paper' && compSelection == 'Scissors') {
-        return 'Computer wins!'
+        return `Computer wins! ${compSelection} beats ${playerSelection}`
      } else if (playerSelection == compSelection) {
         return 'Tie!'
-    }
+    };
 };
 
 // assign user and computer choices to a variable
-const compChoice = getComputerChoice();
-const playerChoice = 'Rock';
+const compSelection = getComputerChoice();
+const playerSelection = 'Rock';
 
-// log the results
-console.log(playGame(playerChoice, compChoice))
 
+// creates a for loop to make rounds which will be logged to the console
+const game = () => {
+    console.log('5 rounds of fun begin now!')
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = 'Rock';
+        const compSelection = getComputerChoice();
+        console.log(playRound(playerSelection, compSelection));
+    }
+};
+game();
 
 
